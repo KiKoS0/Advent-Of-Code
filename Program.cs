04d09ca07545aa4988a4fd5e48e7a0327d820857
KiKoS <riadh.kikos123@hotmail.fr>
2020-12-04 19:33:02 +0100

    Refactor

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using static Util.Utility;
 
+Advent.Advent2020.Day1();
+Advent.Advent2020.Day2();
+Advent.Advent2020.Day3();
 Advent.Advent2020.Day4();
-
-
